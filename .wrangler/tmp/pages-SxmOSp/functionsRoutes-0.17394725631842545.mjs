6f6db20bf4364def9f2ae29607e6cf9cd15de98d
강 틀가 <agape_ibeel@hanpass.com>
2026-03-25 10:05:03 +0900

    Fix layout to ensure back and print buttons are visible in order details, and make stepper interactive

---
+++ .wrangler/tmp/pages-SxmOSp/functionsRoutes-0.17394725631842545.mjs
@@ -0,0 +1,83 @@
+import { onRequestPost as __api_admin_login_js_onRequestPost } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\admin\\login.js"
+import { onRequestPost as __api_auth_login_js_onRequestPost } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\auth\\login.js"
+import { onRequestPost as __api_auth_register_js_onRequestPost } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\auth\\register.js"
+import { onRequestGet as __api_upload_js_onRequestGet } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\upload.js"
+import { onRequestPost as __api_upload_js_onRequestPost } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\upload.js"
+import { onRequest as __api_banners_js_onRequest } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\banners.js"
+import { onRequest as __api_containers_js_onRequest } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\containers.js"
+import { onRequest as __api_init_db_js_onRequest } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\init-db.js"
+import { onRequest as __api_notifications_js_onRequest } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\notifications.js"
+import { onRequest as __api_orders_js_onRequest } from "C:\\Users\\agape_ibeel\\Desktop\\nomin cargo\\functions\\api\\orders.js"
+
+export const routes = [
+    {
+      routePath: "/api/admin/login",
+      mountPath: "/api/admin",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_admin_login_js_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/login",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_login_js_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/register",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_register_js_onRequestPost],
+    },
+  {
+      routePath: "/api/upload",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_upload_js_onRequestGet],
+    },
+  {
+      routePath: "/api/upload",
+      mountPath: "/api",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_upload_js_onRequestPost],
+    },
+  {
+      routePath: "/api/banners",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_banners_js_onRequest],
+    },
+  {
+      routePath: "/api/containers",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_containers_js_onRequest],
+    },
+  {
+      routePath: "/api/init-db",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_init_db_js_onRequest],
+    },
+  {
+      routePath: "/api/notifications",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_notifications_js_onRequest],
+    },
+  {
+      routePath: "/api/orders",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_orders_js_onRequest],
+    },
+  ]
